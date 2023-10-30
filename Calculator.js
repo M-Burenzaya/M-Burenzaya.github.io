47c3eeeb073dd11ea89278f4239d6b8206170d34
Burenzaya <bvrnee1009@gmail.com>
2023-10-30 23:02:49 +0800

    DOM

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,6 @@
 let memoryContainer = document.getElementById("memory-container");
 let array = [];
+
 for (let i = 0; i < 10; i++) {
   let divElement = document.createElement("div");
   divElement.id = `memory-${i}`;
@@ -7,16 +8,34 @@ for (let i = 0; i < 10; i++) {
   memoryContainer.appendChild(divElement);
 }
 let i = 0;
-function Evaluate(args){
-    const value = eval(args);
-    return value;
+function Evaluate(args) {
+  const value = eval(args);
+  return value;
 }
 
-function memoryWrite(args){
-  if(Evaluate(args) == args){
+function memoryWrite(args) {
+  if (Evaluate(args) == args) {
     array.push(args);
     let myElement = document.getElementById(`memory-${i}`);
-    myElement.textContent = "Memory " +array[i];
+    myElement.textContent = "Memory " + array[i];
     i++;
+  }
+}
+
+function scientific() {
+  var table = document.getElementById("myTable");
+  var row = document.getElementById("additionalRow");
+
+  if (row.style.display === "none") {
+    row.style.display = "table-row";
+    row.innerHTML = `
+      <td><input type="button" value="  (  " onclick="display.value+='('"></td>
+      <td><input type="button" value="  )  " onclick="display.value+=')'"></td>
+      <td><input type="button" value=" % " onclick="display.value+='%'"></td>
+      <td><input type="button" value="sqrt(" onclick="display.value+='Math.sqrt('"></td>
+    `;
+  } else {
+    row.style.display = "none";
+    row.innerHTML = "";
   }
 }
