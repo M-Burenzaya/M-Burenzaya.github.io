dbd4095c1f8a48200154dc6199827055d00008ce
Ja-Adam <jadambajaak874@gmail.com>
2023-10-30 20:50:54 +0800

    memory utga hadgalasan

--- Calculator.js
+++ Calculator.js
@@ -1,13 +1,17 @@
 let memoryContainer = document.getElementById("memory-container");
-let array = [0];
+let array = [];
 for (let i = 0; i < 10; i++) {
   let divElement = document.createElement("div");
   divElement.id = `memory-${i}`;
   divElement.textContent = `Memory ${array[i]}`;
   memoryContainer.appendChild(divElement);
 }
-
+let i = 0;
 function Evaluate(args){
     const value = eval(args);
+    array.push(value);
+    let myElement = document.getElementById(`memory-${i}`);
+    myElement.textContent = "Memory " +array[i];
+    i++;
     return value;
 }
