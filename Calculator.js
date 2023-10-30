5cb86149a8253ca5499270ee5efb2e3fa47636ff
Ja-Adam <jadambajaak874@gmail.com>
2023-10-30 20:00:13 +0800

    eval func utga avah

--- Calculator.js
+++ Calculator.js
@@ -1,8 +1,13 @@
 let memoryContainer = document.getElementById("memory-container");
-
+let array = [0];
 for (let i = 0; i < 10; i++) {
   let divElement = document.createElement("div");
   divElement.id = `memory-${i}`;
-  divElement.textContent = `Memory ${i+1}`;
+  divElement.textContent = `Memory ${array[i]}`;
   memoryContainer.appendChild(divElement);
+}
+
+function Evaluate(args){
+    const value = eval(args);
+    return value;
 }
